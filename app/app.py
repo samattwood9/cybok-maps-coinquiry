9b7cfc6ababae0947c945f097ecd0011346dcc2e
Sam Attwood <59017719+samattwood9@users.noreply.github.com>
2023-06-28 13:39:46 +0100

    Update app.py
    - add tooltip to multi-select
    - caution comparisons to mapped degree programmes

--- app/app.py
+++ app/app.py
@@ -33,7 +33,7 @@ for _, row in mappings_df.iterrows():
     ]
 
 # user selection
-selected_titles = st.multiselect("Select generic job titles to view mappings:", title2mapping.keys())
+selected_titles = st.multiselect("Select generic job titles to view mappings:", title2mapping.keys(), help="Note: We do not advise comparing the mapped job titles to mapped degree programmes at this time.")
 
 if len(selected_titles) != 0:
     # make plot
